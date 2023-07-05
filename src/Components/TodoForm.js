0681ba8698d38ecbe0c89678a2774e07baa8c800
SanjanaSoni2021 <sanjanas.bca2021@ssism.org>
2023-07-05 12:54:10 +0530

    created a Todo list application

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function TodoForm(props) {
+    // state
+    const [input,setInput] = useState('');
+
+    const handleChange = e =>{
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        props.addTask({
+            id:Math.floor(Math.random()* 1000000),
+            text:input,
+            isComplete: false
+        })
+        setInput('')
+    }
+  return (
+    <div>
+        <form className='todo-form'>
+            <input type='text' placeholder='Add a todo' onChange={handleChange} className="todo-input" value={input} name='text'/>
+            <button type='submit' onClick={handleSubmit} className='todo-btn'> ADD ToDo</button>
+        </form>
+    </div>
+  )
+}
